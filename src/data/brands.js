e372fb3b670cd603f6626001d4c90475b56244d7
NathanBartolo <144773867+NathanBartolo@users.noreply.github.com>
2026-07-14 13:04:53 +0400

    ui fixed

--- src/data/brands.js
+++ src/data/brands.js
@@ -5,8 +5,8 @@ import prorasoLogo from '../assets/brands/proraso.png';
 import shiseidoLogo from '../assets/brands/shiseido.png';
 
 export const brands = [
-  { id: 'brand-1', name: 'Fino', logo: finoLogo || brandPlaceholder },
-  { id: 'brand-2', name: 'Marvis', logo: marvisLogo || brandPlaceholder },
-  { id: 'brand-3', name: 'Proraso', logo: prorasoLogo || brandPlaceholder },
-  { id: 'brand-4', name: 'Shiseido', logo: shiseidoLogo || brandPlaceholder },
+  { id: 'brand-1', name: '', logo: finoLogo || brandPlaceholder },
+  { id: 'brand-2', name: '', logo: marvisLogo || brandPlaceholder },
+  { id: 'brand-3', name: '', logo: prorasoLogo || brandPlaceholder },
+  { id: 'brand-4', name: '', logo: shiseidoLogo || brandPlaceholder },
 ];
